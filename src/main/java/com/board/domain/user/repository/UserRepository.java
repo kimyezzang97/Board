8e609735f982918ce1b57cbaab58599cf02aa1f3
kimyezzang97 <kimyezzang97@gmail.com>
2025-01-26 17:08:34 +0900

    User Entity, Repository 생성

---
+++ src/main/java/com/board/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.board.domain.user.repository;
+
+import com.board.domain.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    
+}
