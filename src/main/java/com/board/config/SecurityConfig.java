1fb96f76166b91da9acf7052e30230f57601110a
kimyezzang97 <kimyezzang97@gmail.com>
2025-01-26 17:25:43 +0900

    User 계급화(enum), config

--- src/main/java/com/board/config/SecurityConfig.java
+++ src/main/java/com/board/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.board.config;
 
+import com.board.domain.user.entity.UserRoleType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +22,16 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    // 시큐리티 role 수직적 계층 시큐리티에 적용
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.withRolePrefix("ROLE_")
+                .role(UserRoleType.ADMIN.toString()).implies(UserRoleType.USER.toString())
+                .build();
+    }
+
+
+    // 시큐리티 설정
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // csrf 보안 해제 (개발 환경에서 설정시 복잡성)
