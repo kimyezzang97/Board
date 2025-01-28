b2cbef43c8e68207e7e4cdb1a293d02a764c0226
kimyezzang97 <kimyezzang97@gmail.com>
2025-01-28 20:31:47 +0900

    Board 도메인 작성 2

---
+++ src/main/java/com/board/domain/board/dto/BoardRequestDTO.java
@@ -0,0 +1,13 @@
+package com.board.domain.board.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardRequestDTO {
+
+    private String title;
+    private String content;
+}
