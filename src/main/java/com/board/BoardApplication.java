e6c58d732eff54e7e8ea10c5dd5b1d81d6fb328d
kimyezzang97 <kimyezzang97@gmail.com>
2025-02-02 12:15:13 +0900

    SpringBootApplication 어노테이션 추가
    - Transaction
    - Entity Auditing(날짜)

--- src/main/java/com/board/BoardApplication.java
+++ src/main/java/com/board/BoardApplication.java
@@ -2,8 +2,12 @@ package com.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaAuditing // Auditing 기능 활성화
+@EnableTransactionManagement
 public class BoardApplication {
 
 	public static void main(String[] args) {
