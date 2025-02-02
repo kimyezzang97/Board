37276edd1bff220e2c1c09da10f2a1b34452d650
kimyezzang97 <kimyezzang97@gmail.com>
2025-02-02 12:17:41 +0900

    Spring security 및 JWT 적용
    - CustomLogoutFilter (로그아웃)
    - JWTUtil (JWT 발급)
    - JWTFilter
    - LoginFilter
    - SecurityConfig(bcrypt, 경로 인가 설정)
    
    ** 추후 refresh 토큰 제거 스케줄러 추가 혹은 redis 전환 좋을 듯
    - CustomUserDetailsService
    - CustomUserDetails

---
+++ src/main/java/com/board/member/service/CustomUserDetails.java
@@ -0,0 +1,65 @@
+package com.board.member.service;
+
+import com.board.entity.Member;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    public CustomUserDetails(Member member) {
+
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+
+            @Override
+            public String getAuthority() {
+
+                return member.getRole();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/board/member/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.board.member.service;
+
+import com.board.entity.Member;
+import com.board.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private MemberRepository memberRepository;
+
+    @Autowired
+    public CustomUserDetailsService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> userData = memberRepository.findByUsername(username);
+
+        if (userData.isPresent()) {
+
+            return new CustomUserDetails(userData.get());
+        }
+
+        return null;
+    }
+}
