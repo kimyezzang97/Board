6cec2df25679f61926d35cb0b2c6d2b6665ead75
kimyezzang97 <kimyezzang97@gmail.com>
2025-02-15 12:01:40 +0900

    fix: loadUserByUsername 존재하지 않을 경우 null 수정

--- src/main/java/com/board/member/request/JoinRequest.java
+++ src/main/java/com/board/member/request/JoinRequest.java
@@ -1,8 +1,6 @@
 package com.board.member.request;
 
-import com.board.constant.MemberRole;
 import com.board.entity.Member;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
--- src/main/java/com/board/member/service/CustomUserDetailsService.java
+++ src/main/java/com/board/member/service/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package com.board.member.service;
 
-import com.board.entity.Member;
 import com.board.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -22,13 +20,9 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Member> userData = memberRepository.findByUsername(username);
 
-        if (userData.isPresent()) {
-
-            return new CustomUserDetails(userData.get());
-        }
-
-        return null;
+        return memberRepository.findByUsername(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저의 이름을 찾을 수 없습니다 : " + username)) ;
     }
 }
