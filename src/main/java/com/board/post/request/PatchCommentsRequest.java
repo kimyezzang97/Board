80378847725966cfffe5cb6efd5563400aeae45b
kimyezzang97 <kimyezzang97@gmail.com>
2025-03-09 10:46:20 +0900

    feat: 댓글 수정 request 메서드 수정

--- src/main/java/com/board/post/request/PatchCommentsRequest.java
+++ src/main/java/com/board/post/request/PatchCommentsRequest.java
@@ -1,6 +1,7 @@
 package com.board.post.request;
 
 import com.board.entity.Board;
+import com.board.entity.Comments;
 import com.board.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,8 +14,8 @@ public class PatchCommentsRequest {
 
     private String content;
 
-    public Board to(Member member){
-        return Board.builder()
+    public Comments to(Member member){
+        return Comments.builder()
                 .content(content)
                 .member(member)
                 .build();
